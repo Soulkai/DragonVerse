46ac546199f2aa3469d5904a10602977f67e9761
Soulkai <kaiurysouza55@gmail.com>
2026-04-29 14:35:57 -0400

    Delete src directory

--- src/utils/admin.js
+++
@@ -1,9 +0,0 @@
-const settings = require('../config/settings');
-const { getSenderNumber } = require('./text');
-
-function isAdmin(message) {
-  const sender = getSenderNumber(message);
-  return settings.adminNumbers.includes(sender);
-}
-
-module.exports = { isAdmin };
